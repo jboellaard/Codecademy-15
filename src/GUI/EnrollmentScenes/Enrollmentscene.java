d7bdc3892e46bdfc69b6027906c4ac36892d2d9f
Tim van Vliet <timvanvliet538@gmail.com>
2022-01-13 22:13:47 +0100

    uploading EnrollmentScenes to GUI

---
+++ src/GUI/EnrollmentScenes/Enrollmentscene.java
@@ -0,0 +1,66 @@
+public class GUI {
+    package GUI;
+
+import DB.*;
+import Domain.*;
+
+import javafx.application.Application;
+// import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+// import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        BorderPane layout = new BorderPane();
+        GridPane gridPane = new GridPane();
+
+        Label signUpDate = new Label("Sign up date:");
+        DatePicker courseSUP = new DatePicker();
+        Label course = new Label("course:");
+        TextField courseInput = new TextField();
+        Label studentName = new Label("Student name:");
+        TextField studentNameInput = new TextField();
+
+        Button newStudent = new Button("submit form");
+        newStudent.setOnAction((event) -> {
+            Date sUP = new Date(String.valueOf(courseSUP.getValue()));
+            String course = courseInput.getText();
+            String student = new Email(studentNameInput.getText());
+            
+            EnrollmentRepo repo = new EnrollmentRepo();
+            Enrollment math = new Enrollment(signUpDate, course, student);
+            repo.create(hans);
+            gridPane.add(new Label(""+repo.getAllStudents()),0,7);
+        });
+        
+        Button createStudent = new Button("Create enrollment");
+        gridPane.add(createStudent,0,0);
+        createStudent.setOnAction((event) -> {
+            gridPane.add(signUpDate,0,1);
+            gridPane.add(courseSUP,1,1);
+            gridPane.add(course,0,2);
+            gridPane.add(courseInput,1,2);
+            gridPane.add(studentName,0,3);
+            gridPane.add(studentNameInput,1,3);
+            gridPane.add(createEnrollment,0,6);
+
+        });
+
+        layout.setCenter(gridPane);
+
+        Scene view = new Scene(layout, 600, 600);
+        stage.setTitle("GUI");
+        stage.setScene(view);
+        stage.show();
+    }
+
+    
+}
+
+}
