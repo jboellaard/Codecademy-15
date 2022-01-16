41ead6b3ccd1d4cc2769c5c78169a6553252df43
AfroNeko <lisatyem@gmail.com>
2022-01-16 19:58:58 +0100

    Completed UrlTools and Tests

--- src/Domain/Tools/Tests/UrlToolsTest.java
+++ src/Domain/Tools/Tests/UrlToolsTest.java
@@ -1,5 +1,80 @@
 package Domain.Tools.Tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+import Domain.Tools.UrlTools;
+
 public class UrlToolsTest {
-    
+
+    @Test
+    public void testValidUrlRequiresNoHttpOrHttpsEnsuresFalse() {
+        // arrange
+        String url = "www.a.b";
+
+        // act
+        boolean result = UrlTools.formatUrl(url);
+
+        // assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testValidUrlRequiresOnlyOneDotEnsuresFalse() {
+        // arrange
+        String url = "https://www.b";
+
+        // act
+        boolean result = UrlTools.formatUrl(url);
+
+        // assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testValidUrlRequiresLengthAfterSlashBiggerThanOneEnsuresFalse() {
+        // arrange
+        String url = "https://.a.b";
+
+        // act
+        boolean result = UrlTools.formatUrl(url);
+
+        // assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testValidUrlRequiresLengthBetweenDotsBiggerThanOneEnsuresFalse() {
+        // arrange
+        String url = "https://www..b";
+
+        // act
+        boolean result = UrlTools.formatUrl(url);
+
+        // assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testValidUrlRequiresLengthAfterLastDotBiggerThanOneEnsuresFalse() {
+        // arrange
+        String url = "https://www.a.";
+
+        // act
+        boolean result = UrlTools.formatUrl(url);
+
+        // assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testValidUrlRequiresHttpOrHttpsEnsuresTrue() {
+        // arrange
+        String url = "http://www.url.com";
+
+        // act
+        boolean result = UrlTools.formatUrl(url);
+
+        // assert
+        assertEquals(true, result);
+    }
 }
--- src/Domain/Tools/UrlTools.java
+++ src/Domain/Tools/UrlTools.java
@@ -1,5 +1,21 @@
 package Domain.Tools;
 
 public class UrlTools {
-    
+
+    // URL’s hebben de volgende vorm: <https:// of http://><minimaal één let-
+    // ter>.<minimaal één letter>.<minimaal één letter>
+
+    public static boolean formatUrl(String url) {
+        url = url.trim();
+        if (!url.contains("https://") || !url.contains("http://")) {
+            return false;
+        } else if (url.split("//")[1].length() < 1) {
+            return false;
+        } else if (url.split("\\.")[1].split("\\.")[0].length() < 1) {
+            return false;
+        } else if (url.split("\\.")[1].split("\\.")[1].length() < 1) {
+            return false;
+        }
+        return true;
+    }
 }
