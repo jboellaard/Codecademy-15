3a3f98c166e446d32aaf790f4c9c7fdd8a0958b7
2182556 <je.boellaard@student.avans.nl>
2022-01-16 16:54:41 +0100

    made a small change to gradetools

--- src/Domain/Tools/GradeTools.java
+++ src/Domain/Tools/GradeTools.java
@@ -3,7 +3,7 @@ package Domain.Tools;
 public class GradeTools {
 
     public static boolean isValidGrade(double grade){
-        return false;
+        return 1.00 >= grade && grade <= 10.0;
     }
     
 }
